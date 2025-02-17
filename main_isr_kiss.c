adced520d8f2ddf9c65f4708fe946227c9379d9b
zorrzi <eduardovz@al.insper.edu.br>
2025-02-16 23:59:08 -0300

    Update main_isr_kiss.c

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -2,32 +2,34 @@
 
 int g_cnt = 0;
 char g_str[10];
-int batata;
+int volatile g_flag;
 
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
 void btn_callback(void) {
   printf("btn pressed \n");
+  g_flag = 1;
+  
+}
 
+void main(void) {
+  
   if (g_cnt >= 8)
     g_cnt = 0;
 
-  g_cnt = 2;
-
+if(g_flag == 1){
+  g_flag = 0;
+  
   int i = 0;
   for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
     g_str[i] = '*';
-    g_str[i + 1] = NULLL;
     delay_ms(50);
     gfx_mono_draw_string(g_str, 0, 0, &sysfont);
   }
 }
+  
+  
 
-void main(void) {
-  // ...
-
-  batata = batata + 1;
   while (1) {
   }
 }
